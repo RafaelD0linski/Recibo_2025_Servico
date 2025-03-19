1c10ea8a5cb45ca6986602766de7e8f9bba78375
rafae <rafaeldolinski14@gmail.com>
2025-03-18 21:16:13 -0300

    Ajuste e implementação da classe de mensagem em andamento

--- Recibo_2025_Servico/ReciboServicoUseCase/DadosServicoRequest.cs
+++ Recibo_2025_Servico/ReciboServicoUseCase/DadosServicoRequest.cs
@@ -6,4 +6,4 @@ using System.Threading.Tasks;
 
 namespace Recibo_2025_Servico.ReciboServicoUseCase;
 
-public sealed record class DadosServicoRequest( DateTime dataDoServico, string tipoServico, string descricaoServico, double valorServico, string tipoPagamento );
+public sealed record class DadosServicoRequest( string tipoServico, string descricaoServico, double valorServico, string tipoPagamento );
--- Recibo_2025_Servico/ReciboServicoUseCase/Form1.cs
+++ Recibo_2025_Servico/ReciboServicoUseCase/Form1.cs
@@ -1,3 +1,4 @@
+using Recibo_2025_Servico.MensagemDeAvisoUseCase;
 using Recibo_2025_Servico.ReciboServicoUseCase;
 
 namespace Recibo_2025_Servico;
@@ -17,16 +18,18 @@ public partial class InformacoesServico : Form
     public void ObterDadosServico()
     {
         DadosServicoRequest request = new(
-            Convert.ToDateTime(datadoservico),
+           // Convert.ToDateTime(datadoservico),
             txtTipoServico.Text,
             txtDescricaoServico.Text,
             txtValorServicoPrestado.Text.Length > 0 ? Convert.ToDouble(txtValorServicoPrestado.Text) : 0,
             txtTipoPagamento.Text
             );
 
-        if (request is null)
-          
-            return;
+        if (request.descricaoServico == string.Empty )
+        {
+            MensagemAviso mensagem = new("Erro", "Exitem campos incompletos", NivelAviso.Critico);
+            mensagem
+        }
 
         _dadosServico = request;
 
@@ -35,10 +38,6 @@ public partial class InformacoesServico : Form
 
     private void btnImprimir_Click(object sender, EventArgs e)
     {
-        if (_dadosServico is null)
-            return;
-
-
-
+        ObterDadosServico();
     }
 }
