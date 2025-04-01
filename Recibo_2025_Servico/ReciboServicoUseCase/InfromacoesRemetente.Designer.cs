b9946da7ff0cb3481155f11e6a5e16cba4f438f8
rafae <rafaeldolinski14@gmail.com>
2025-04-01 20:33:18 -0300

    Tela de infrormaçõesdo remetente

---
+++ Recibo_2025_Servico/ReciboServicoUseCase/InfromacoesRemetente.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Recibo_2025_Servico.ReciboServicoUseCase
+{
+    partial class InfromacoesRemetente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            txtNome = new TextBox();
+            label2 = new Label();
+            txtTelefone = new TextBox();
+            label3 = new Label();
+            txtEmail = new TextBox();
+            txtcpfcnpj = new TextBox();
+            label4 = new Label();
+            txtCidade = new TextBox();
+            txtRua = new TextBox();
+            txtEstado = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            txtCep = new TextBox();
+            txtNumero = new TextBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label10 = new Label();
+            label11 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(212, 383);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Proximo";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(12, 69);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(158, 27);
+            txtNome.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 109);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Telefone";
+            // 
+            // txtTelefone
+            // 
+            txtTelefone.Location = new Point(12, 132);
+            txtTelefone.Name = "txtTelefone";
+            txtTelefone.Size = new Size(158, 27);
+            txtTelefone.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(212, 109);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 20);
+            label3.TabIndex = 5;
+            label3.Text = "E-mail";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(212, 132);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(228, 27);
+            txtEmail.TabIndex = 6;
+            // 
+            // txtcpfcnpj
+            // 
+            txtcpfcnpj.Location = new Point(212, 69);
+            txtcpfcnpj.Name = "txtcpfcnpj";
+            txtcpfcnpj.Size = new Size(173, 27);
+            txtcpfcnpj.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(212, 46);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 20);
+            label4.TabIndex = 8;
+            label4.Text = "CPF/CNPJ";
+            // 
+            // txtCidade
+            // 
+            txtCidade.Location = new Point(12, 257);
+            txtCidade.Name = "txtCidade";
+            txtCidade.Size = new Size(125, 27);
+            txtCidade.TabIndex = 9;
+            // 
+            // txtRua
+            // 
+            txtRua.Location = new Point(212, 323);
+            txtRua.Name = "txtRua";
+            txtRua.Size = new Size(246, 27);
+            txtRua.TabIndex = 10;
+            // 
+            // txtEstado
+            // 
+            txtEstado.Location = new Point(12, 383);
+            txtEstado.Name = "txtEstado";
+            txtEstado.Size = new Size(125, 27);
+            txtEstado.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 234);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 20);
+            label5.TabIndex = 12;
+            label5.Text = "Cidade";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(213, 300);
+            label6.Name = "label6";
+            label6.Size = new Size(34, 20);
+            label6.TabIndex = 13;
+            label6.Text = "Rua";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 360);
+            label7.Name = "label7";
+            label7.Size = new Size(54, 20);
+            label7.TabIndex = 14;
+            label7.Text = "Estado";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(212, 234);
+            label8.Name = "label8";
+            label8.Size = new Size(35, 20);
+            label8.TabIndex = 15;
+            label8.Text = "Cep";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(12, 300);
+            label9.Name = "label9";
+            label9.Size = new Size(63, 20);
+            label9.TabIndex = 16;
+            label9.Text = "Numero";
+            // 
+            // txtCep
+            // 
+            txtCep.Location = new Point(212, 257);
+            txtCep.Name = "txtCep";
+            txtCep.Size = new Size(125, 27);
+            txtCep.TabIndex = 18;
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(12, 323);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(125, 27);
+            txtNumero.TabIndex = 19;
+            txtNumero.TextChanged += textBox9_TextChanged;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.AppWorkspace;
+            pictureBox1.Location = new Point(-4, -30);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(482, 73);
+            pictureBox1.TabIndex = 20;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = SystemColors.AppWorkspace;
+            pictureBox2.Location = new Point(-4, 174);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(482, 47);
+            pictureBox2.TabIndex = 21;
+            pictureBox2.TabStop = false;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = SystemColors.AppWorkspace;
+            label10.Location = new Point(135, 9);
+            label10.Name = "label10";
+            label10.Size = new Size(163, 20);
+            label10.TabIndex = 22;
+            label10.Text = "Informações remetente";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = SystemColors.AppWorkspace;
+            label11.Location = new Point(136, 189);
+            label11.Name = "label11";
+            label11.Size = new Size(147, 20);
+            label11.TabIndex = 23;
+            label11.Text = "Endereço Remetente";
+            label11.Click += label11_Click;
+            // 
+            // InfromacoesRemetente
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(470, 423);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(txtNumero);
+            Controls.Add(txtCep);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(txtEstado);
+            Controls.Add(txtRua);
+            Controls.Add(txtCidade);
+            Controls.Add(label4);
+            Controls.Add(txtcpfcnpj);
+            Controls.Add(txtEmail);
+            Controls.Add(label3);
+            Controls.Add(txtTelefone);
+            Controls.Add(label2);
+            Controls.Add(txtNome);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "InfromacoesRemetente";
+            Text = "InfromacoesRemetente";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox txtNome;
+        private Label label2;
+        private TextBox txtTelefone;
+        private Label label3;
+        private TextBox txtEmail;
+        private TextBox txtcpfcnpj;
+        private Label label4;
+        private TextBox txtCidade;
+        private TextBox txtRua;
+        private TextBox txtEstado;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox txtCep;
+        private TextBox txtNumero;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label10;
+        private Label label11;
+    }
+}
---
+++ Recibo_2025_Servico/ReciboServicoUseCase/InfromacoesRemetente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recibo_2025_Servico.ReciboServicoUseCase
+{
+    public partial class InfromacoesRemetente : Form
+    {
+        public InfromacoesRemetente()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
