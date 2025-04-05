6440a09842ee3f895039309e994bc6ab9ccdc8ca
rafae <rafaeldolinski14@gmail.com>
2025-04-04 21:13:04 -0300

    Ajuste na geração de pdf

--- Recibo_2025_Servico/ReciboServicoUseCase/FrmInformacoeServico.cs
+++ Recibo_2025_Servico/ReciboServicoUseCase/FrmInformacoeServico.cs
@@ -60,45 +60,6 @@ public partial class InformacoesServico : Form
     }
 
 
-    public void GerarPdf()
-    {
-        try
-        {
-            // Caminho onde o PDF será salvo (pasta do projeto)
-            string caminho = Path.Combine(Application.StartupPath, "MeuArquivo.pdf");
-
-            // Criar o documento
-            Document doc = new Document(PageSize.A4);
-            PdfWriter.GetInstance(doc, new FileStream(caminho, FileMode.Create));
-
-            doc.Open();
-            doc.Add(new Paragraph("Olá, este é um PDF gerado pelo meu programa!"));
-
-            doc.Add(new Paragraph("Nome: João da Silva"));
-            doc.Add(new Paragraph("Data de Nascimento: 10/05/1995"));
-            doc.Add(new Paragraph("Endereço: Rua das Flores, 123, Centro"));
-
-
-
-            PdfPTable tabela = new PdfPTable(2); // 2 colunas
-            tabela.AddCell("Nome:");
-            tabela.AddCell("João da Silva");
-            tabela.AddCell("Data de Nascimento:");
-            tabela.AddCell("10/05/1995");
-            tabela.AddCell("Endereço:");
-            tabela.AddCell("Rua das Flores, 123, Centro");
-
-            doc.Close();
-
-            Process.Start(new ProcessStartInfo(caminho) { UseShellExecute = true });
-
-            MessageBox.Show("PDF gerado e aberto com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show("Erro ao gerar PDF: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-    }
 
     private async void ObterdadosApi(object sender, EventArgs e)
     {
--- Recibo_2025_Servico/ReciboServicoUseCase/FrmInformacoesDevedor.cs
+++ Recibo_2025_Servico/ReciboServicoUseCase/FrmInformacoesDevedor.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +18,52 @@ namespace Recibo_2025_Servico.ReciboServicoUseCase
         public InformacoesDevedor()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GerarPdf();
+            //Validar opção de usar o quest pdf para gerar o pdf 
+        }
+
+        public void GerarPdf()
+        {
+            try
+            {
+                // Caminho onde o PDF será salvo (pasta do projeto)
+                string caminho = Path.Combine(Application.StartupPath, "MeuArquivo.pdf");
+
+                // Criar o documento
+                Document doc = new Document(PageSize.A4);
+                PdfWriter.GetInstance(doc, new FileStream(caminho, FileMode.Create));
+
+                doc.Open();
+                doc.Add(new Paragraph("Olá, este é um PDF gerado pelo meu programa!"));
+
+                doc.Add(new Paragraph("Nome: João da Silva"));
+                doc.Add(new Paragraph("Data de Nascimento: 10/05/1995"));
+                doc.Add(new Paragraph("Endereço: Rua das Flores, 123, Centro"));
+
+
+
+                PdfPTable tabela = new PdfPTable(2); // 2 colunas
+                tabela.AddCell("Nome:");
+                tabela.AddCell("João da Silva");
+                tabela.AddCell("Data de Nascimento:");
+                tabela.AddCell("10/05/1995");
+                tabela.AddCell("Endereço:");
+                tabela.AddCell("Rua das Flores, 123, Centro");
+
+                doc.Close();
+
+                Process.Start(new ProcessStartInfo(caminho) { UseShellExecute = true });
+
+                MessageBox.Show("PDF gerado e aberto com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao gerar PDF: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
