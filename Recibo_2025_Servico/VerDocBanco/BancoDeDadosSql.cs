098883c182c3688b0a36d50d746079b3f4c9e2ac
rafae <rafaeldolinski14@gmail.com>
2025-04-04 21:14:52 -0300

    Inicio do banco de dados

---
+++ Recibo_2025_Servico/VerDocBanco/BancoDeDadosSql.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recibo_2025_Servico.VerDocBanco
+{
+    internal class BancoDeDadosSql
+    {
+    }
+}
